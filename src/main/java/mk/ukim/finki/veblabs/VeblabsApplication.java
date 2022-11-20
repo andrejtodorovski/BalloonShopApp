31ec3d427cca4bdc015c33647bce6f48cfd182fc
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2022-11-21 00:28:45 +0100

    Starting the app

---
+++ src/main/java/mk/ukim/finki/veblabs/VeblabsApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.veblabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeblabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeblabsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/veblabs/VeblabsApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.veblabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeblabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
